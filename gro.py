c81544e94d0ccc02efbfc2dfc307dd11ac1df132
Andrew Kabala <kabalaandrew@gmail.com>
2015-12-17 19:03:46 +0300

    Switched shelve keys to represent to represent the loops

--- gro.py
+++ gro.py
@@ -1,12 +1,19 @@
 
-import shelve
-s = shelve.open('drows.db')
-try:
-	s['drows'] = {1:'foo', 2:'bar', 3:'LumberJack', 4:'I work all day'}
-finally:
-	s.close()	
+def main():
+	import shelve
+	#make our data persistent
+	s = shelve.open('drows.db')
+	try:
+		#created first dict and made a dict of dicts
+		s['foo'] = {1:'foo', 2:'bar', 3:'LumberJack', 4:'I work all day'}
+		s['bar'] = {1:'foo', 2:'bar', 3:'LumberJack', 4:'I work all day'}
+	finally:
+		s.close()	
 
-s = shelve.open('drows.db')	
-drow = s['drows']
-for item in drow:
-	print(drow[item])
+	s = shelve.open('drows.db')	
+	
+	for item in s:
+		print(s[item])
+
+if __name__ == "__main__":
+	main()
